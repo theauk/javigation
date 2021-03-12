b34da14ac21845c691e6693856bcf6fcba460c2e
maod <maod@itu.dk>
2021-03-12 15:32:55 +0100

    Basic node and Way

--- app/src/main/java/bfst21/Osm_Elements/Node.java
+++ app/src/main/java/bfst21/Osm_Elements/Node.java
@@ -1,5 +1,25 @@
 package bfst21.Osm_Elements;
 
 public class Node extends Element{
+    private float x, y;
+    private transient long id;
+
+    public Node(long id, float lat, float lon) {
+        this.id = id;
+        this.x = lon;
+        this.y = -lat/0.56f;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+	public long getID() {
+		return id;
+	}
     
 }
--- app/src/main/java/bfst21/Osm_Elements/Way.java
+++ app/src/main/java/bfst21/Osm_Elements/Way.java
@@ -1,5 +1,25 @@
 package bfst21.Osm_Elements;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Way extends Element {
+    private List<Node> nodes = new ArrayList<>();
+    private Long id;
+
+    public Node first() {
+        return nodes.get(0);
+    }
+    
+    public void add(Node node) {
+        nodes.add(node);
+    }
+
+    public void setId(Long _id){
+        id = _id;
+    }
+    public Long getId(){
+        return id;
+    } 
     
 }
