b5b79cdacaaac60e5ad9b30934ab5f0069f91982
maod <maod@itu.dk>
2021-03-16 14:38:33 +0100

    Nodes can be in KDTree

--- app/src/main/java/bfst21/Osm_Elements/Node.java
+++ app/src/main/java/bfst21/Osm_Elements/Node.java
@@ -3,6 +3,9 @@ package bfst21.Osm_Elements;
 public class Node extends Element{
     private float x, y;
     private transient long id;
+    private Node leftChild;
+    private Node rightChild;
+    private Boolean OnXAxis;
 
     public Node(long id, float lat, float lon) {
         this.id = id;
@@ -21,5 +24,33 @@ public class Node extends Element{
 	public long getID() {
 		return id;
 	}
+
+    public Node getLeftChild() {
+        return leftChild;
+    }
+
+    public void setLeftChild(Node leftChild) {
+        this.leftChild = leftChild;
+    }
+
+    public Node getRightChild() {
+        return rightChild;
+    }
+
+    public void setRightChild(Node rightChild) {
+        this.rightChild = rightChild;
+    }
+
+    public Boolean IsOnXAxis() {
+        return OnXAxis;
+    }
+
+    public void setIsOnXAxis(Boolean xAxis) {
+        this.OnXAxis = xAxis;
+    }
+
+   
+
+    
     
 }
