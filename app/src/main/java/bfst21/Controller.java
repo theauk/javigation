5547fe16e8e5da5c081a08e7ab833079d1980ac7
Gustav <gume@itu.dk>
2021-03-18 12:54:50 +0100

    Implemented better zoom functionality and zoom level.
    Added: Geo coordinates are now available for display in the footer.
    
    Implemented: You can now zoom via the scale bar, scrolling or clicking at the items Zoom in/out in the menubar (plus with CTRL + + and CTRL + -)

--- app/src/main/java/bfst21/Controller.java
+++ app/src/main/java/bfst21/Controller.java
@@ -2,13 +2,17 @@ package bfst21;
 
 import bfst21.view.MapCanvas;
 import bfst21.view.MapSegment;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.Point2D;
 import javafx.scene.Cursor;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.Slider;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.ScrollEvent;
+import javafx.scene.transform.NonInvertibleTransformException;
 import javafx.stage.FileChooser;
 
 import java.io.File;
@@ -16,30 +20,107 @@ import java.io.File;
 public class Controller
 {
     private MapSegment mapSegment;
-    private Point2D currentMouse;
-    private Point2D lastMouse;
     private Loader osmLoader;
 
-    @FXML
-    private MapCanvas mapCanvas;
-    @FXML
-    private Scene scene;
-    @FXML
-    private Label coordsLabel;
+    private Point2D currentMouse;
+    private Point2D lastMouse;
+
+    private int zoomLevel;
+    private final int MAX_ZOOM_LEVEL = 100;
+    private final int MIN_ZOOM_LEVEL = 0;
+    private final int ZOOM_FACTOR = 32;
+    private boolean viaSlider = true;
+
+    @FXML private MapCanvas mapCanvas;
+    @FXML private Scene scene;
+    @FXML private Label coordsLabel;
+    @FXML private Label geoCoordsLabel;
+    @FXML private Slider zoomSlider;
+    @FXML private MenuItem zoomInItem;
+    @FXML private MenuItem zoomOutItem;
 
     public void init(MapSegment mapSegment)
     {
         this.mapSegment = mapSegment;
         mapCanvas.init(mapSegment);
         osmLoader = new Loader();
-        openFile();
+
+        initView();
+
+        //openFile();
     }
 
+    private void initView()
+    {
+        zoomSlider.setValue(zoomLevel);
+        zoomSlider.setMax(MAX_ZOOM_LEVEL);
+        zoomSlider.setMin(MIN_ZOOM_LEVEL);
+        zoomSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
+            if(viaSlider)
+            {
+                if(newValue.intValue() > oldValue.intValue()) zoom(ZOOM_FACTOR);
+                else if(newValue.intValue() < oldValue.intValue()) zoom(-ZOOM_FACTOR);
+            }
+        });
+    }
+
+    /**
+     * Called when a ScrollEvent is raised. Zooms in on the MapCanvas at the Mouse's coordinates.
+     * @param e The ScrollEvent associated with the caller.
+     */
     @FXML
     public void onScroll(ScrollEvent e)
     {
-        double factor = Math.pow(1.01, e.getDeltaY());
-        mapCanvas.zoom(factor, new Point2D(e.getX(), e.getY()));
+        viaSlider = false;
+        zoom(e.getDeltaY(), new Point2D(e.getX(), e.getY()));
+    }
+
+    /**
+     * Zoom in or out on the MapCanvas via the MenuBar.
+     * @param e The ActionEvent associated with the caller.
+     */
+    @FXML
+    private void zoom(ActionEvent e)
+    {
+        viaSlider = false;
+        if(e.getSource().equals(zoomInItem)) zoom(ZOOM_FACTOR);
+        else if(e.getSource().equals(zoomOutItem)) zoom(-ZOOM_FACTOR);
+    }
+
+    /**
+     * Zooms in or out on the MapCanvas at the center point of the Canvas.
+     * @param amount The zoom amount.
+     */
+    private void zoom(double amount)
+    {
+        zoom(amount, new Point2D(mapCanvas.getWidth() / 2, mapCanvas.getHeight() / 2));
+    }
+
+    /**
+     * Zooms in or out on the MapCanvas within a certain zoom level at a certain point.
+     * @param amount The zoom amount.
+     * @param center The zoom center as a 2D point.
+     */
+    private void zoom(double amount, Point2D center)
+    {
+        if(zoomLevel >= MIN_ZOOM_LEVEL && zoomLevel <= MAX_ZOOM_LEVEL)
+        {
+            double factor = Math.pow(1.01, amount);
+
+            if(amount > 0 && zoomLevel != MAX_ZOOM_LEVEL)
+            {
+                zoomLevel++;
+                mapCanvas.zoom(factor, center);
+            }
+            else if(amount < 0 && zoomLevel != MIN_ZOOM_LEVEL)
+            {
+                zoomLevel--;
+                mapCanvas.zoom(factor, center);
+            }
+        }
+
+        if(!viaSlider) zoomSlider.setValue(zoomLevel);
+        viaSlider = true;
     }
 
     @FXML
@@ -49,7 +130,6 @@ public class Controller
         double dy = e.getY() - lastMouse.getY();
 
         mapCanvas.setCursor(Cursor.CLOSED_HAND);
-
         if(e.isPrimaryButtonDown()) mapCanvas.pan(dx, dy);
 
         currentMouse = new Point2D(e.getX(), e.getY());
@@ -73,7 +153,7 @@ public class Controller
     }
 
     @FXML
-    public void onMouseReleased(MouseEvent e)
+    public void onMouseReleased()
     {
         mapCanvas.setCursor(Cursor.DEFAULT);
     }
@@ -82,6 +162,8 @@ public class Controller
     public void resetView()
     {
         mapCanvas.reset();
+        zoomLevel = MIN_ZOOM_LEVEL;
+        zoomSlider.setValue(MIN_ZOOM_LEVEL);
         setCoordsLabel(currentMouse);
     }
 
@@ -111,10 +193,22 @@ public class Controller
 
     private void setCoordsLabel(Point2D point)
     {
-        Point2D coords = mapCanvas.getTransCoords(point.getX(), point.getY());
+        Point2D coords = null;
+        Point2D geoCoords = null;
+        try {
+            coords = mapCanvas.getTransCoords(point.getX(), point.getY());
+            geoCoords = mapCanvas.getGeoCoords(point.getX(), point.getY());
+        } catch (NonInvertibleTransformException e) {
+            e.printStackTrace();
+        }
+
         double x = Math.round(coords.getX() * 10) / 10.0;
         double y = Math.round(coords.getY() * 10) / 10.0;
-        coordsLabel.setText("Coords: " + x + ", " + y);
+        coordsLabel.setText("CanvasCoords: " + x + ", " + y);
+
+        x = Math.round(geoCoords.getX() * 10000000) / 10000000.0;
+        y = Math.round(geoCoords.getY() * 10000000) / 10000000.0;
+        geoCoordsLabel.setText("GeoCoords: " + x + ", " + y);
     }
 
     private FileChooser showFileChooser()
