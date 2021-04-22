55b4a183682428e10105a44962379e951ae8fb67
Gustav <gume@itu.dk>
2021-04-22 10:27:26 +0200

    Changed: Made changes to what text is displayed via getMessage().
    Now contains which zip file failed to load.

--- app/src/main/java/bfst21/exceptions/NoOSMInZipFileException.java
+++ app/src/main/java/bfst21/exceptions/NoOSMInZipFileException.java
@@ -1,8 +1,15 @@
 package bfst21.Exceptions;
 
 public class NoOSMInZipFileException extends Exception {
+    private final String fileName;
 
-    public NoOSMInZipFileException(String message) {
-        super(message);
+    public NoOSMInZipFileException(String fileName) {
+        super(" does not contain a zipped OSM file.");
+        this.fileName = fileName.substring(fileName.lastIndexOf("\\") + 1);
+    }
+
+    @Override
+    public String getMessage() {
+        return "\"" + fileName + "\"" + super.getMessage();
     }
 }
