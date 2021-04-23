8bb3a50b6e9704276d4f2e9650709061250fe640
moad <maod@itu.dk>
2021-04-23 08:44:10 +0200

    Added helpful comments and java docs to KDTree

--- app/src/main/java/bfst21/data_structures/KDTree.java
+++ app/src/main/java/bfst21/data_structures/KDTree.java
@@ -29,7 +29,6 @@ public class KDTree<Value extends Element> implements Serializable {
     private int startDim;
     private int numCor;
     private int numDim;
-    private boolean noDuplicates;
     private HashSet<Value> hashList;
 
     public KDTree(int startDim, int numCor) {
@@ -58,16 +57,21 @@ public class KDTree<Value extends Element> implements Serializable {
         buildTree(list, startDim);
     }
 
+    /**
+     * Recursively builds the tree with the list provided. Method only gets called by buildTree().
+     * Uses comparators on the list for every recursive call to make sure the tree is built evenly,
+     *
+     * @param nodes List of nodes that the tree should be built from.
+     * @param dim   dimension, meaning x or y axis,
+     * @return returns a KDTreeNode
+     */
     private KDTreeNode buildTree(List<KDTreeNode> nodes, int dim) {
         if (nodes.isEmpty()) {
             return null;
         }
 
-        Comparator<KDTreeNode> comp = getComparatorFromDimension(dim % numCor);
+        Comparator<KDTreeNode> comp = getComparatorFromDimension(dim % numCor); // Chooses x or y comparator.
         nodes.sort(comp);
-        if (!noDuplicates) {
-            noDuplicates = true;
-        }
 
         int med = getMedian(0, nodes.size());
         KDTreeNode medNode = nodes.get(med);
@@ -77,62 +81,80 @@ public class KDTree<Value extends Element> implements Serializable {
             root = medNode;
         }
 
-        medNode.leftChild = buildTree(nodes.subList(0, med), dim + numDim);
-        medNode.rightChild = buildTree(nodes.subList(med + 1, nodes.size()), dim + numDim);
+        medNode.leftChild = buildTree(nodes.subList(0, med), dim + numDim);              // builds the left subtree return the child to this node.
+        medNode.rightChild = buildTree(nodes.subList(med + 1, nodes.size()), dim + numDim); // builds the right subtree return the child to this node.
 
         return medNode;
     }
 
-
+    /**
+     * @param x coordinate
+     * @param y coordinate
+     * @return Nearest value
+     * @throws KDTreeEmptyException throws if the tree is empty
+     */
     public Value getNearestNode(float x, float y) throws KDTreeEmptyException {
 
         KDTreeNode nearestNode = getNearestNode(x, y, root, null);
         return nearestNode.node;
     }
 
+    /**
+     * Helper method for getNearest Node
+     * Calculates whether or not it is worth checking the 'other side' of the tree for possible closer node.
+     * It returns true if the shortest distance is larger than the distance between x or y to the current nodes axis.
+     */
     private boolean possibleCloserNode(Double shortestDistance, KDTreeNode currentNode, float x, float y) {
         double possibleNewDistance = Math.abs(currentNode.onXAxis ? x - currentNode.node.getxMax() : y - currentNode.node.getyMax());
         return shortestDistance > Math.abs(possibleNewDistance);
     }
 
-    private double getDistance(KDTreeNode from, float[] cor) {
+    private double getDistance(KDTreeNode from, float x, float y) {
         if (from == null) {
             return Double.POSITIVE_INFINITY;
         } else {
-            Point2D p = new Point2D(cor[0], cor[1]);
+            Point2D p = new Point2D(x, y);
             return p.distance(from.node.getxMax(), from.node.getyMax());
         }
     }
 
+    /**
+     * Recursively finds the nearest node, determining the most promising side and looking there first.
+     * @param x coordinate
+     * @param y coordinate
+     * @param currentNode node to search
+     * @param nearestNode nearest node so far.
+     * @return returns the nearest node
+     */
     private KDTreeNode getNearestNode(float x, float y, KDTreeNode currentNode, KDTreeNode nearestNode) {
         KDTreeNode currentNearest = nearestNode;
 
-        if (currentNode == null) {
+        if (currentNode == null) { // end of tree
             return currentNearest;
         }
 
-        double currentDistance = getDistance(currentNode, new float[]{x, y});
-        double minimumDistance = getDistance(currentNearest, new float[]{x, y});
+        double currentDistance = getDistance(currentNode, x, y);
+        double minimumDistance = getDistance(currentNearest, x, y);
 
-        if (currentDistance < minimumDistance) {
+        if (currentDistance < minimumDistance) {  // if currentnode is closer than currentNearest
             currentNearest = currentNode;
         }
 
-        boolean isCoordinateLessThan;
+        boolean isCoordinateLessThan;  // finds if the most promising side of the tree is left or right
         if (currentNode.onXAxis) {
             isCoordinateLessThan = x < currentNode.node.getxMax();
         } else {
             isCoordinateLessThan = y < currentNode.node.getyMax();
         }
 
-        if (isCoordinateLessThan) {
+        if (isCoordinateLessThan) { // searches the most promising side first then checks if it is worth checking the other side.
             currentNearest = getNearestNode(x, y, currentNode.leftChild, currentNearest);
-            if (possibleCloserNode(getDistance(currentNearest, new float[]{x, y}), currentNode, x, y)) {
+            if (possibleCloserNode(getDistance(currentNearest, x, y), currentNode, x, y)) {
                 currentNearest = getNearestNode(x, y, currentNode.rightChild, currentNearest);
             }
         } else {
             currentNearest = getNearestNode(x, y, currentNode.rightChild, currentNearest);
-            if (possibleCloserNode(getDistance(currentNearest, new float[]{x, y}), currentNode, x, y)) {
+            if (possibleCloserNode(getDistance(currentNearest, x, y), currentNode, x, y)) {
                 currentNearest = getNearestNode(x, y, currentNode.leftChild, currentNearest);
             }
         }
