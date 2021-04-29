3210301c5f4f1131e23b4e899343c06f9741a2b5
Charlotte <csbe@itu.dk>
2021-04-29 15:17:01 +0200

    Edited some javadoc in the AddressTriesTree

--- app/src/main/java/bfst21/data_structures/AddressTriesTree.java
+++ app/src/main/java/bfst21/data_structures/AddressTriesTree.java
@@ -26,6 +26,8 @@ public class AddressTriesTree implements Serializable {
      * @param postcode    -> The 4 digit number that tells in what part of the country the address is located.
      *                    in Denmark it's how far they are from Copenhagen
      * @param houseNumber -> the housenumber or street-number, that the address had
+     *
+     *                    calls insert that inserts the node into the trie via its streetname and city.
      */
     public void put(Node node, String city, String streetname, int postcode, String houseNumber) {
         insert(root, node, city, streetname, postcode, houseNumber);
@@ -38,16 +40,19 @@ public class AddressTriesTree implements Serializable {
         }
 
     /**
-     * This will insert the address into the trie by using its postcode as key.
-     * @param trieNode -> when called for the first time, this would be the root.
-     *                 afterwards in the recursive calls inside the method will call the method with the next node (a child), and proceed
-     *                 to the bottom of the trie, where the addressNode will be added to the Arraylist in that last node's arraylist.
-     *                 this @param trieNode could be omitted, but then the methods needs to be iterative instead of recursive.
      *
+     * @param trieNode -> when called for the first time, this would be the root.
+     *      *                 afterwards in the recursive calls inside the method will call the method with the next node (a child), and proceed
+     *      *                 to the bottom of the trie, where the addressNode will be added to the Arraylist in that last node's arraylist.
+     *      *                 this @param trieNode could be omitted, but then the methods needs to be iterative instead of recursive.
      * @param index -> the start index is always 0, since the method will start from the root, and go down through the tree.
-     *              index could be omitted as well, but the method would need to be made iterative instead of recursive.
+     *      *              index could be omitted as well, but the method would need to be made iterative instead of recursive.
+     * @param node -> the node that contains the coordinates for the address
+     * @param city -> the name of the city given by the .osm file.
+     * @param streetname -> the name of the street given by the .osm file.
+     * @param postcode -> the postcode of the node given by the .osm file.
+     * @param houseNumber -> the house number given by the .osm file.
      */
-
     private void insert_address_with_streetname(AddressTrieNode trieNode, int index, Node node, String city, String streetname, int postcode, String houseNumber){
         if (index == streetname.length()) {
                 if(trieNode.isAddress()) trieNode.addHouseNumber(city, node, houseNumber);
@@ -63,12 +68,19 @@ public class AddressTriesTree implements Serializable {
             }
     }
 
-    // Returns all streetnames in the trie.
+    /**
+     *
+     * @return -> returns all the possible streetnames in the trie.
+     */
     public ArrayList<AddressTrieNode> keys(){
         return searchWithPrefix("");
     }
-    // Returns AddressTrieNodes with streetnames that has the given prefix
-    // (taken from the algo book, however it has been modified)
+
+    /**
+     * Adapted from the Algorithms book by Sedgewick & Wayne
+     * @param prefix -> prefix to possible streetnames
+     * @return -> a list (ArrayList) with the possible streetnames that matches the prefix with help from the help methods.
+     */
     public ArrayList<AddressTrieNode> searchWithPrefix(String prefix){
         ArrayList<AddressTrieNode> queue = new ArrayList<>();
         prefix = prefix.toLowerCase();
@@ -76,8 +88,15 @@ public class AddressTriesTree implements Serializable {
         return queue;
     }
 
+    /**
+     *
+     * @param trieNode -> the trienode the method is currently at. It starts from the root and goes through the tree until it
+     *                 gets the node associated with key in the subtrie rooted at the trienode
+     * @param key -> could also be called prefix. Calls the method recursively until index is as long as key, and then it returns the given trienode
+     * @param index -> starts at 0, but increases up to the length of the key - this is so the method can be called recursively
+     * @return -> returns the trienode that matches the key.
+     */
     private AddressTrieNode get(AddressTrieNode trieNode, String key, int index){
-        // return node associated with key in the subtrie rooted at trienode
         if(trieNode == null) return null;
         if(index == key.length()) {
             return trieNode;
@@ -86,6 +105,13 @@ public class AddressTriesTree implements Serializable {
         return get(trieNode.getChildren().get(character),key, index+1);
     }
 
+    /**
+     *
+     * @param trieNode -> the trienode the method is currently at. It starts from the root and goes through the tree until it
+     *                 finds a streetname that matches the given prefix and add it to the list (queue), simply returns nothing.
+     * @param prefix -> help method for searchWithPrefix -> adds streetnames to the list that begins with the given prefix
+     * @param queue -> the list that searchWithPrefix with possible streetnames to the given prefix
+     */
     private void collect(AddressTrieNode trieNode, String prefix, ArrayList<AddressTrieNode> queue){
         if(trieNode == null) return;
             if(trieNode.isAddress()){
@@ -95,27 +121,5 @@ public class AddressTriesTree implements Serializable {
         for (Map.Entry<Character, AddressTrieNode> child : trieNode.getChildren().entrySet()) {
             collect(child.getValue(), prefix + child.getKey(), queue);
         }
-    }
-
-    // quick print test
-    public static void main(String[] args) {
-        Node node1 = new Node(340551927, 55.6786770f, 12.5694510f);
-        Node node2 = new Node(340551928, 55.6786400f, 12.5698360f);
-        Node node3 = new Node(340551929,55.6786500f,12.5698370f);
-        Node node4 = new Node(340551930,55.6783500f,12.5693370f);
-
-        AddressTriesTree trie = new AddressTriesTree();
-        trie.put(node1, "København K", "Studiestræde", 1455, "18");
-        trie.put(node2, "København K", "Studiestræde", 1455, "19");
-        trie.put(node3, "København K", "Studievej", 1455, "25");
-        trie.put(node4, "Roskilde", "Studiestræde", 4000, "4");
-
-
-        ArrayList<AddressTrieNode> list = trie.searchWithPrefix("Studie");
-        for(AddressTrieNode node : list){
-            System.out.println(node.getStreetname());
-        }
-
-
     }
         }
